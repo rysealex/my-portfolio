7ab45f5558abb6dbb0e6938192508734625c944e
Alex Ryse <aryse54@gmail.com>
2025-11-14 00:06:14 -0800

    Updated profile pic.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className='home-container'>
       <div className='pic-container'>
-        <img src='profile_pic.png' alt='profile_pic'></img>
+        <img src='upd_profile_pic.jpg' alt='profile_pic'></img>
       </div>
       <h2>Alex Ryse</h2>
       <h3>Computer Science Undergraduate Student</h3>
