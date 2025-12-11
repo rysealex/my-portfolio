1432f3522bf9e10e1b0f07f00fc60b5c949473ef
Alex Ryse <aryse54@gmail.com>
2025-12-11 14:02:14 -0800

    Updated home section

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
-
 function Home() {
+
+  // handle the enter portfolio button scroll
+  const handleScroll = (e) => {
+    e.preventDefault();
+    
+    const section = document.getElementById('about');
+    const navHeight = 60;
+
+    if (section) {
+      const yOffset = -navHeight;
+      const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth'});
+    }
+  };
+
   return (
     <div className='home-container'>
       <div className='pic-container'>
@@ -8,6 +21,9 @@ function Home() {
       </div>
       <h2>Alex Ryse</h2>
       <h3>Computer Science Undergraduate Student</h3>
+      <div className='enter-container'>
+        <button onClick={(e) => handleScroll(e)}><b>Enter Portfolio</b></button>
+      </div>
     </div>
   );
 }
